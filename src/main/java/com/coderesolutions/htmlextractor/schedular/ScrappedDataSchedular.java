449eab2118c3eb4d76e15bcbf03d2c6142c27957
im-komo <pravesh.tlabs@gmail.com>
2017-11-27 18:35:47 +0530

    update schedular

--- src/main/java/com/coderesolutions/htmlextractor/schedular/ScrappedDataSchedular.java
+++ src/main/java/com/coderesolutions/htmlextractor/schedular/ScrappedDataSchedular.java
@@ -14,20 +14,25 @@ public class ScrappedDataSchedular {
 	@Autowired
 	ScrappedDataService scrappedDataService; 
 	
-//	@Scheduled(fixedDelay = 1000000)
+	@Scheduled(initialDelay = 5000, fixedDelay = 40000)
 	public void schedularOne() {
 		System.out.println(new Date());
+		System.out.println("zero");
+		scrappedDataService.findByZeroStage();
+	}
+	
+	@Scheduled(initialDelay = 10000, fixedDelay = 40000)
+	public void schedularSecond() {
+		System.out.println(new Date());
+		System.out.println("first");
 		scrappedDataService.findByFirstStage();
 	}
 	
-	@Scheduled(fixedDelay = 1000000)
-	public void schedularSecond() {
-		System.out.println(new Date());		
-	}
-	
-	@Scheduled(fixedDelay = 1000000)
+	@Scheduled(initialDelay = 20000, fixedDelay = 40000)
 	public void schedularThird() {
-		System.out.println(new Date());		
+		System.out.println(new Date());
+		System.out.println("second");
+		scrappedDataService.findBySecondStage();
 	}
 
 }
